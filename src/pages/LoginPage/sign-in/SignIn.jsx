52aa0e44ef6829b990a51e926cd50b8675997d03
seohwa0325 <seohwa0325@gmail.com>
2023-08-02 11:41:46 +0900

    redux

--- src/pages/LoginPage/sign-in/SignIn.jsx
+++ src/pages/LoginPage/sign-in/SignIn.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
 import Form from '../../../components/form/Form'
 import { useNavigate } from 'react-router-dom'
-import {createUserWithEmailAndPassword ,getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import app from '../../../firebase'
+import { useDispatch } from 'react-redux'
+import { setUser } from '../../../store/user/user.slice'
+// import { setUserId } from '../../../store/cart/cart.slice';
+
 
 const SignIn = () => {
 
   const navigate =useNavigate()
   const [firebaseError , setFirebaseError] =useState("")
   const auth =getAuth(app);
-
+  const dispatch = useDispatch()
   const handleLogin =(email, password) =>{
     signInWithEmailAndPassword(auth, email, password)
-    .then(user =>{
+    .then(userCredential =>{
+      dispatch(setUser({
+        email: userCredential.user.email,
+        token: userCredential.user.refreshToken,
+        id: userCredential.user.uid
+      }))
+      // dispatch(setUserId(userCredential.user.uid));
+
       navigate('/')
     }).catch(error =>{
       return error && setFirebaseError("이메일 또는 비밀번호가 잘못되었습니다.")
--- src/pages/RegisterPage/sign-up/SignUp.jsx
+++ src/pages/RegisterPage/sign-up/SignUp.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
 import Form from '../../../components/form/Form'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
 import {createUserWithEmailAndPassword ,getAuth} from 'firebase/auth'
 import app from '../../../firebase'
+// import { setUserId } from '../../../store/cart/cart.slice';
+import { setUser } from '../../../store/user/user.slice';
 const SignUp = () => {
   const navigate =useNavigate()
   const [firebaseError , setFirebaseError] =useState("")
   const auth =getAuth(app);
+  const dispatch = useDispatch();
 
   const handleSignupAndLogin =(email, password) =>{
    
     createUserWithEmailAndPassword(auth , email , password)
-    .then((user) =>{
-      //리덕스
-      navigate('/')
-    })
+    .then(userCredential => {
+      dispatch(setUser({
+          email: userCredential.user.email,
+          token: userCredential.user.refreshToken,
+          id: userCredential.user.uid
+      }))
+      // dispatch(setUserId(userCredential.user.uid));
+      navigate('/');
+  })
     .catch((error) =>{
       return error && setFirebaseError("이메일 또는 비밀번호가 잘못되었습니다.")
     })
